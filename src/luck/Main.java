f8d4bedae460ddb4cb407b2f751235070a80e0a4
airemons <airemonstv551378@gmail.com>
2021-04-14 23:26:53 +0530

    Game

---
+++ src/luck/Main.java
@@ -0,0 +1,13 @@
+package luck;
+
+public class Main {
+
+    static void fiftyPercentChanceFunction() {
+        double random = Math.random();
+        System.out.println(random);
+    }
+
+    public static void main (String[] args) {
+        fiftyPercentChanceFunction();
+    }
+}
