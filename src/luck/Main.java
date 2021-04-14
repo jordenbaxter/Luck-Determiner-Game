c7077ea713ad259504a5626648e3a78f2be10638
airemons <airemonstv551378@gmail.com>
2021-04-14 23:37:57 +0530

    Game

--- src/luck/Main.java
+++ src/luck/Main.java
@@ -2,12 +2,27 @@ package luck;
 
 public class Main {
 
+    private static int zeroOrOne;
+
     static void fiftyPercentChanceFunction() {
-        double random = Math.random();
-        System.out.println(random);
+        int random = (int) Math.round(Math.random() * 10);
+        if (random <= 5) {
+            zeroOrOne = 0;
+        } else {
+            zeroOrOne = 1;
+        }
     }
 
     public static void main (String[] args) {
         fiftyPercentChanceFunction();
+        System.out.println(zeroOrOne);
+        fiftyPercentChanceFunction();
+        System.out.println(zeroOrOne);
+        fiftyPercentChanceFunction();
+        System.out.println(zeroOrOne);
+        fiftyPercentChanceFunction();
+        System.out.println(zeroOrOne);
+        fiftyPercentChanceFunction();
+        System.out.println(zeroOrOne);
     }
 }
